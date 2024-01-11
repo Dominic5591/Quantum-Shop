c56b9a1ad9f61f38dcdbbd3da3d2e9788d3c04c1
Dominic <domcashiola@gmail.com>
2024-01-11 17:14:34 -0500

    SessionController

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::SessionsController < ApplicationController
+  def show
+    @user = current_user
+    if @user
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+  end
+
+
+  def create
+    credential = params[:credential]
+    password = params[:password]
+
+    @user = User.find_by_credentials(credential, password)
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['credentials invalid!'] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: { message: 'logged out' }
+    end
+  end
+end
