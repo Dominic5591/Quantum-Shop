632d4551667c0b93f3da6c54d70a03c8be14a8c0
Dominic <domcashiola@gmail.com>
2024-01-11 17:06:40 -0500

    Setting up application controller and seed file

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_validation :ensure_session_token
+  has_secure_password
+  validates :username, uniqueness: true, length: { in: 3..40 }, format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+  validates :email, uniqueness: true, length: { in: 3..100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..40 }, allow_nil: true
+
+
+  
+  def self.find_by_credentials(credential, password)
+    field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+    user = User.find_by(field => credential)
+    if user&.authenticate(password) 
+      return user
+    else
+      nil 
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    while true
+      token = SecureRandom.urlsafe_base64
+      return token unless User.exists?(session_token: token)
+  end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+end
