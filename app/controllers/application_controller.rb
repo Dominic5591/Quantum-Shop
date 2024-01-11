6696d3131ff9e749647773aeedac50b4a597eb2a
Dominic <domcashiola@gmail.com>
2024-01-11 16:50:39 -0500

    backend setup again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
 
 
-  const HELLO = "hello"
+  before_action :snake_case_params
+
+  private
+
+  def snake_case_params
+    params.deep_transform_keys!(&:underscore)
+  end
 end
