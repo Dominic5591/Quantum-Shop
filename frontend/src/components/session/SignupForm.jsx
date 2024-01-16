6eccc41406fe51587ddac73145c1c88cf735e6e8
Dominic <domcashiola@gmail.com>
2024-01-16 15:22:04 -0500

    Completed styling signup and login pages

--- frontend/src/components/session/SignupForm.jsx
+++ frontend/src/components/session/SignupForm.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, NavLink } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
 import arrow2 from '../../images/arrow2.png';
+import git from '../../images/github.png';
+import linkedin from '../../images/linkedin.png';
 import './SignupForm.css';
 
 
@@ -98,11 +100,23 @@ function SignupForm() {
 
           <p className='signupP'>Already have an account?<NavLink className='existingAcc' to='/login'> Sign in</NavLink></p>
         </form>
-
-
-        
       </div>
       <div className="createAccountDivider"></div>
+
+      <div className='loginLinks'>
+        <span className='loginGit'>
+          <a href="https://github.com/Dominic5591">
+            <img src={git} alt="" />
+          </a>
+        </span>
+        <span className='loginLinkedin'>
+          <a href="https://www.linkedin.com/in/dominic-c-1076322a8/">
+            <img src={linkedin} alt="" />
+          </a>
+          
+        </span>
+        <p className='loginLinkP'>2024 QuantumShop</p>
+      </div>
     </div>
   );
 }
