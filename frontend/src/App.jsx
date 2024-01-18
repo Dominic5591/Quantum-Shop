a63299a19c40982ba3c9f4811f75d7cfe367b1e5
Dominic <domcashiola@gmail.com>
2024-01-18 11:55:45 -0500

    Working on react-router error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,7 @@ import { Outlet, createBrowserRouter, RouterProvider, useLocation } from 'react-
 import LoginForm from './components/session/LoginForm';
 import SignupForm from './components/session/SignupForm';
 import Navigation from './components/navigation/Navigation';
-// import HomePage from './components/homepage/HomePage';
 import * as sessionActions from './store/session';
-
 
 import ProductsIndex from './components/product/ProductsIndex';
 import ProductIndexItem from './components/product/ProductIndexItem';
@@ -16,7 +14,6 @@ function Layout() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [showNavigation, setShowNavigation] = useState(true);
   const location = useLocation();
-  // const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(sessionActions.restoreSession()).then(() => {
@@ -25,10 +22,7 @@ function Layout() {
   }, [dispatch]);
 
   useEffect(() => {
-    // Check if the current location is '/login' or '/signup'
-    const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
-
-    // Update showNavigation state based on the current location
+    const isAuthPage = ['/login', '/signup'].includes(location.pathname);
     setShowNavigation(!isAuthPage);
   }, [location.pathname]);
 
@@ -40,29 +34,33 @@ function Layout() {
   );
 }
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: 'login',
-        element: <LoginForm />,
-      },
-      {
-        path: 'signup',
-        element: <SignupForm />,
-      },
-      {
-        path: 'products',
-        element: <ProductsIndex />,
-      },
-      {
-        path: "products/:productId",
-        element: <ProductIndexItem />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter({
+  routes: [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: 'login',
+          element: <LoginForm />,
+        },
+        {
+          path: 'signup',
+          element: <SignupForm />,
+        },
+        {
+          index: 'products',
+          element: <ProductsIndex />,
+        },
+        {
+          path: 'products/:productId',
+          element: <ProductIndexItem />,
+        },
+      ],
+    },
+  ],
+});
+
 
 
 function App() {
