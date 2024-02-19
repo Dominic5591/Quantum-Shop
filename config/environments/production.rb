d93aaea5e09e0ecb66df4cfd2812a94b0a07068f
Dominic <domcashiola@gmail.com>
2024-02-19 14:33:25 -0500

    deploying to render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  config.action_mailer.default_url_options = { host: "https://quantumshop.onrender.com" }
-  Rails.application.routes.default_url_options[:host] = "https://quantumshop.onrender.com"
-  config.require_master_key = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
