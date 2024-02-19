59825b4ab571ba035289bf3d54e0b4236bb7eec1
Dominic <domcashiola@gmail.com>
2024-02-19 16:42:24 -0500

    deploying to render

--- config/initializers/active_storage.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
-Rails.application.routes.default_url_options[:host] = 'quantumshop.onrender.com'
-ActiveStorage::Current.url_options = Rails.application.routes.default_url_options
