2872f219598bf0b4d1ef82cc326aafd7fc7ed9e7
Dominic <domcashiola@gmail.com>
2024-04-06 13:23:56 -0400

    fixing issues with Render deployment

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates: :rating, :title, :product_id, :user_id, presence: true
+  validates :rating, :title, :product_id, :user_id, presence: true
 
   belongs_to :product
   belongs_to :user
