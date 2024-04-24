80f122c0e43912b9c8243189084f63750d968c4d
Dominic <domcashiola@gmail.com>
2024-04-23 20:41:58 -0400

    deploying to render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-# exit on error
+# Exit immediately if a command exits with a non-zero status
 set -o errexit
+# Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -o nounset
+# Prevent errors in a pipeline from being masked
+set -o pipefail
 
+# Install gems
 bundle install
+
+# Run database migrations
 rails db:migrate
-rails db:reset #if needed
+
+# Reset database (use with caution)
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
