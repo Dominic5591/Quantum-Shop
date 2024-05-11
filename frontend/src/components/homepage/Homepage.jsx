7bf88dc9e17117baf616cbde3abb75e6aa35856a
Dominic <domcashiola@gmail.com>
2024-05-10 21:36:17 -0400

    working with react-infinite-scroll-component library on homepage

--- frontend/src/components/homepage/Homepage.jsx
+++ frontend/src/components/homepage/Homepage.jsx
@@ -1,25 +1,22 @@
+import { useEffect, useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchProducts, selectProductsArray } from '../../store/product';
 import ProductGrid from '../carousel/ProductGrid';
 import CategoryCarousel from '../carousel/CategoryCarousel';
 import homepageBanner from '../../images/homepageBanner.jpg';
 import Footer from '../footer/Footer';
+import InfiniteScroll from 'react-infinite-scroll-component'; // Import InfiniteScroll
 import 'react-loading-skeleton/dist/skeleton.css';
 import './Homepage.css';
 
-
 import SkeletonHomepage from '../skeleton/SkeletonHomepage';
-
 
 const Homepage = () => {
   const products = useSelector(selectProductsArray);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [loaded, setLoaded] = useState(false);
-
-
   const [page, setPage] = useState(1);
 
   useEffect(() => {
@@ -36,51 +33,50 @@ const Homepage = () => {
     fetchData();
   }, [dispatch, page]);
 
-  const loadMoreProducts = () => {
+  const handleScroll = useCallback(() => {
+    const bottomOfPage = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight;
+    if (bottomOfPage) {
+      setPage(prevPage => prevPage + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
+  const loadMore = async () => {
     setPage(prevPage => prevPage + 1);
   };
-  
 
   if (!loaded) {
-    return (
-      <SkeletonHomepage />
-    );
+    return <SkeletonHomepage />;
   }
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    navigate('./categories/home');
-  };
-  
-
-  console.log(products);
 
   return (
     <div className="pageContainer">
-      <div className='homeBannerContainer'>
-        <img src={homepageBanner} alt="Homepage Banner" className="homepageBanner" onClick={handleClick}/>
+        <div className='homeBannerContainer'>
+          <img src={homepageBanner} alt="Homepage Banner" className="homepageBanner" onClick={() => navigate('./categories/home')} />
+        </div>
+        <div className='homepageMain'>
+        <InfiniteScroll
+          scrollableTarget="homepageMain"
+          dataLength={products.length} // This is important field to render the next data
+          next={loadMore} // Required - A function which gets executed when you reach to the bottom of this list
+          hasMore={true} // A boolean indicating whether there are more items to load
+          loader={<h1>loading</h1>} // A UI loader to display while loading more posts
+          scrollThreshold={0.5}
+        >
+          <CategoryCarousel products={products} category="books" message="Recommended books for you"/>
+          <ProductGrid products={products} productRange="0, 16"/>
+          <CategoryCarousel products={products} category="fashion" message="Trending Fashion"/>
+          <ProductGrid products={products} productRange="16, 32"/>
+          <CategoryCarousel products={products} category="electronics" message="Recommended electronics"/>
+          <ProductGrid products={products} productRange="32, 48"/>
+    </InfiniteScroll>
+        </div>
+        <Footer />
       </div>
-      <div className='homepageMain'>
-        <CategoryCarousel products={products} category="books" message="Recommended books for you"/>
-
-
-        <ProductGrid products={products} productRange="0, 16" />
-
-
-        <CategoryCarousel products={products} category="fashion" message="Trending Fashion"/>
-
-
-        <ProductGrid products={products} productRange="16, 32" />
-
-
-        <CategoryCarousel products={products} category="electronics" message="Recommended electronics"/>
-
-      </div>
-      {products && products.length > 0 && (
-        <button onClick={loadMoreProducts}>Load More</button>
-      )}
-      <Footer />
-    </div>
   );
 };
 
