ffeb954579dc2c15dab8838fd7d42a61b219497f
Dominic <domcashiola@gmail.com>
2024-05-24 16:55:03 -0400

    fixed issue with require_logged_in for cart_items and orders

--- frontend/src/components/cart/CartIndex.jsx
+++ frontend/src/components/cart/CartIndex.jsx
@@ -118,8 +118,8 @@ const CartIndex = () => {
             <NavLink className='checkoutBtn' to=''>Checkout</NavLink>
           }
         </div>
-      </div>
       <Footer />
+      </div>
     </>
 
   );
